df16b39db40057caa5e80f185c20c409ae7a9fc1
Alvin Prenda Laroya <53330353+alvinlaroya@users.noreply.github.com>
2025-08-22 17:04:45 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,16 +11,16 @@ export default defineNuxtConfig({
   shopify: {
     name: 'journee-collection',
     clients: {
-      storefront: {
+      /* storefront: {
         apiVersion: '2025-07',
         publicAccessToken: '0390f216afb6c9f013e15bdf2e24519d',
-      },
+      }, */
 
-      /* storefront: {
+      storefront: {
         mock: true,
 
         apiVersion: '2025-07',
-      }, */
+      },
 
       /* admin: {
         apiVersion: '2025-07',
